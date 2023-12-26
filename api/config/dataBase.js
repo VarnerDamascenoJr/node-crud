b5d063943b499248807bb72ed40e1011a4a3cf0e
VarnerDamascenoJr <varnerdjr@gmail.com>
2023-12-26 08:05:48 -0300

    config for env file

--- api/config/dataBase.js
+++ api/config/dataBase.js
@@ -1,8 +1,9 @@
+import 'dotenv/config'
 import mysql from 'mysql'
 
 export const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'host',
-  password: 'Password@#!123',
-  database: 'test'
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 })
