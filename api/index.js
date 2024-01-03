d320e646a1d2760bdc3960405c32865fd4e90a26
VarnerDamascenoJr <varnerdjr@gmail.com>
2024-01-03 18:42:31 -0300

    inital config of backend

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
+import cors from 'cors'
+import { backEndTestAlive, getAllBooks } from './controller/book.js'
 const app = express()
 
-app.get('/', (req, res) => {
-  res.json('Backend is alive!!')
-})
+app.use(express.json())
+app.use(cors())
+
+app.get('/', backEndTestAlive)
+app.get('/books', getAllBooks)
 
 app.listen(process.env.PORT, () => console.log('Backend is working'))
