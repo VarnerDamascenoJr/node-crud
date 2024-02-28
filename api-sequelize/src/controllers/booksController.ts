da5acd3e9aa4eb48ac76e1cfb34cccff782a801f
VarnerDamascenoJr <varnerdjr@gmail.com>
2024-02-28 13:35:37 -0300

    features: update and delete

--- api-sequelize/src/controllers/booksController.ts
+++ api-sequelize/src/controllers/booksController.ts
@@ -16,8 +16,54 @@ async function findBook(req: Request, res: Response) {
   res.json(book);
 }
 
+async function createBook(req: Request, res: Response) {
+  const { title, desc, cover, price, createdAt, updatedAt } = req.body;
+  const book = { title, desc, cover, price, createdAt, updatedAt }
+
+  await booksModels.create(book);
+
+  res.json(book)
+}
+
+async function deleteBook(req: Request, res: Response) {
+  const bookId = req.params.id;
+
+
+  await booksModels.destroy({
+    where: {
+      id: bookId
+    }
+  })
+
+  res.json('Book deleted')
+}
+
+async function updateBook(req: Request, res: Response) {
+  const bookId = req.params.id;
+
+
+  const { title, desc, cover, price } = req.body;
+
+
+
+  const book = {
+    title: title,
+    desc: desc,
+    cover: cover,
+    price: price,
+  }
+
+  await booksModels.update(book, {
+    where: {
+      id: bookId,
+    }
+  })
+}
 
 export {
   findAllBooks,
-  findBook
+  findBook,
+  createBook,
+  deleteBook,
+  updateBook,
 }
